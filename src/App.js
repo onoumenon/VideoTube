c0b3e7aaeb332d1b2a3e46358c06702eb0757d76
huitian <tanhuitian@gmail.com>
2019-05-13 16:17:45 +0800

    App with searchbar, video list and video detail

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import { searchYoutube } from "./api/youtube";
+import VideoList from "./VideoList";
+import WithErrorAndLoading from "./WithErrorAndLoading";
+import VideoDetail from "./VideoDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { videos: [], selectedVideo: null, isLoading: false, error: null };
+
+  componentDidMount() {
+    this.onTermSubmit("javascript");
+  }
+
+  onTermSubmit = async term => {
+    this.setState({ isLoading: true });
+    try {
+      const results = await searchYoutube(term);
+      this.setState({
+        isLoading: false,
+        videos: results,
+        selectedVideo: results[0]
+      });
+    } catch (err) {
+      this.setState({ isLoading: false, error: err.message });
+    }
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui stackable grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail selectedVideo={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              {WithErrorAndLoading(
+                this.state.isLoading,
+                this.state.error,
+                <VideoList
+                  onVideoSelect={this.onVideoSelect}
+                  videos={this.state.videos}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
